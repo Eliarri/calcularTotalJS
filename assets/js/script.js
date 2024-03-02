2e7a36f4f69d3e0479da8cf3cb59b13deea19524
“Eliarri” <arriagada.elias@gmail.com>
2024-03-02 14:39:28 -0300

    subiendo

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+const precio = 400000;
+const precioSpan = document.querySelector('.precio-inicial');
+precioSpan.innerHTML = precio;
+
+function calcularPrecioTotal(cantidad, precio) {
+    const amount = Number(cantidad);
+    const price = Number(precio);
+
+    const total = price * amount;
+
+    return Intl.NumberFormat('en-ES', {
+        style: 'currency',
+        currency: 'CLP',
+        currencySign: 'accounting',
+        maximumFractionDigits: 2,
+    }).format(total);
+
+}
+
+function aumentar() {
+    const precioTotalElement = document.querySelector('.valor-total');
+    const cantidadElement = document.querySelector('.cantidad');
+    const cantidadValue = Number(document.querySelector('.cantidad').innerHTML);
+    const nuevaCantidad = cantidadValue + 1;
+
+    cantidadElement.innerHTML = nuevaCantidad;
+
+    const valorTotal = calcularPrecioTotal(nuevaCantidad, precio);
+
+    precioTotalElement.innerHTML = valorTotal;
+
+}
+
+function restar() {
+    const precioTotalElement = document.querySelector('.valor-total');
+    const cantidadElement = document.querySelector('.cantidad');
+    const cantidadValue = Number(document.querySelector('.cantidad').innerHTML);
+
+    if (cantidadValue > 0) {
+        const nuevaCantidad = cantidadValue - 1;
+        cantidadElement.innerHTML = nuevaCantidad - 1;
+
+        const valorTotal = calcularPrecioTotal(nuevaCantidad, precio);
+        precioTotalElement.innerHTML = valorTotal;
+    }
+}
